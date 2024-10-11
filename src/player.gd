a47e444650bef2a36219f2b6c918a3c3fef72fcd
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-10-11 17:27:14 +0000

    player movements + walls + clamping player

--- src/player.gd
+++ src/player.gd
@@ -2,8 +2,17 @@ extends CharacterBody2D
 
 const _SPEED: float = 300.0
 
+# ! WARNING DO NOT CHANGE THESE! WARNING !
+# 30.0 is wall offset, (1200, 800) is length of arena.
+const _UPLEFT_BOUND: Vector2 = Vector2.ONE * 30.0
+const _DOWNRIGHT_BOUND: Vector2 = Vector2(1200, 800) - _UPLEFT_BOUND
+
+
 func _physics_process(delta: float) -> void:
-	
-	# unnormalized vector is fairer when every key press costs
+	# unnormalized vector is fairer when EVERY key press is costly
 	position.x += Input.get_axis(&"left", &"right") * _SPEED * delta
 	position.y += Input.get_axis(&"up", &"down") * _SPEED * delta
+
+	# stays in arena
+	position = position.clamp(_UPLEFT_BOUND, _DOWNRIGHT_BOUND)
+	
