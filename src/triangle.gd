7853aa957d0758eab923c5f15f55f93c8bf188f5
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-10-12 20:45:11 +0000

    enemies spawn via main.gd + font

--- src/triangle.gd
+++ src/triangle.gd
@@ -9,6 +9,9 @@ const _DOWNRIGHT_BOUND: Vector2 = Vector2(1200, 800) - _UPLEFT_BOUND
 
 @onready var _player: = $/root/Main/Player as Node2D
 
+func _spawn(pos: Vector2) -> void:
+	position = pos
+
 func _physics_process(delta: float) -> void:
 	var direction: Vector2 = _player.position - global_position
 	var angle: float = transform.x.angle_to(direction) - QUART
