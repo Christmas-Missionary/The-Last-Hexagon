457e15bdd83e1a9a6514a90397db05b847291d44
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-10-11 17:07:05 -0700

    Added bullet functionality

--- src/bullet.gd
+++ src/bullet.gd
@@ -1,6 +1,13 @@
 extends Area2D
 
+const SPEED: float = 600.0
+
 var velocity: Vector2 = Vector2()
+
+func spawn(pos: Vector2, dir: Vector2):
+	velocity = dir * SPEED
+	pos += (velocity / 20)
+	position = pos
 
 func _physics_process(delta: float) -> void:
 	position += velocity * delta
