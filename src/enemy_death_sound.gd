31ec5f31d87e10a021a7ea05eb1d4049f0e4e3fb
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-10-13 22:30:40 +0000

    random pitch + button sound + energy used in menu

--- src/enemy_death_sound.gd
+++ src/enemy_death_sound.gd
@@ -1,7 +1,14 @@
 extends AudioStreamPlayer2D
 class_name EnemyDeathSound
 
+static var _template: = AudioStreamRandomizer.new()
+
+static func _static_init() -> void:
+	_template.random_pitch = 2.0
+
 func spawn(sound: AudioStream, pos: Vector2) -> void:
-	stream = sound
+	var to_insert: = _template
+	to_insert.add_stream(0, sound)
+	stream = to_insert
 	position = pos
 	play()
---
+++ src/energy_used.gd
@@ -0,0 +1,22 @@
+extends RichTextLabel
+
+var _energy_used: float = 0
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouse:
+		_energy_used += 0.015625
+	if event is InputEventKey:
+		_energy_used += 0.125
+	if event.is_action_pressed(&"shoot"):
+		_energy_used += 0.75
+	
+	text = "Energy used in menu: " + str(_energy_used).substr(0, 9)
+
+func _turn_off() -> void:
+	set_process_input(false)
+	hide()
+	_energy_used = 0
+
+func _turn_on() -> void:
+	set_process_input(true)
+	show()
