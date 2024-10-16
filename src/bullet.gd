5cacd189842ac058ebfa5f8e933afb3118e8c95c
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-10-16 15:02:00 -0700

    Fixed bullets getting stuck on edge

--- src/bullet.gd
+++ src/bullet.gd
@@ -1,6 +1,8 @@
 extends Area2D
 class_name Bullet
 
+const BULLET_UPLEFT: Vector2 = Vector2.ONE * 10.0
+const BULLET_DOWNRIGHT: Vector2 = Vector2(1190, 790)
 const SPEED: float = 600.0
 
 var velocity: Vector2 = Vector2()
@@ -12,16 +14,16 @@ func spawn(pos: Vector2, dir: Vector2) -> void:
 	body_entered.connect(_hit_body)
 	velocity = dir * SPEED
 	pos += (velocity / 20)
-	position = pos
+	position = pos.clamp(Main.UPLEFT_BOUND, Main.DOWNRIGHT_BOUND)
 
 func _physics_process(delta: float) -> void:
 	position += velocity * delta
 	
 	# Check to see if bullet will bounce
 	var last_vel: Vector2 = velocity
-	if Main.UPLEFT_BOUND.y >= position.y or Main.DOWNRIGHT_BOUND.y <= position.y:
+	if BULLET_UPLEFT.y >= position.y or BULLET_DOWNRIGHT.y <= position.y:
 		velocity.y = -velocity.y
-	if Main.UPLEFT_BOUND.x >= position.x or Main.DOWNRIGHT_BOUND.x <= position.x:
+	if BULLET_UPLEFT.x >= position.x or BULLET_DOWNRIGHT.x <= position.x:
 		velocity.x = -velocity.x
 	if last_vel != velocity and Main.is_playing:
 		_bounce_sound.play()
