d8ddaf4b51eb45ec92e435bf0b1bca67884832e8
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-10-16 17:04:16 -0700

    time label simplified

--- src/time_label.gd
+++ src/time_label.gd
@@ -1,15 +1,6 @@
 extends RichTextLabel
 
-const _just_seconds: String = "SS.T"
-const _seconds_minutes: String = "MM:SS.T"
-const _all_three: String = "H:MM:SS.T"
-
-@onready var main: = $"/root/Main" as Main
-
-func _ready() -> void:
-	text = ".0"
-
 func _physics_process(_delta) -> void:
 	if Main.is_playing:
-		var tenths_of_seconds: String = str(roundi(main.time_played * 10))
+		var tenths_of_seconds: String = str(roundi(Main.time_played * 10))
 		text = tenths_of_seconds.insert(tenths_of_seconds.length() - 1, '.')
