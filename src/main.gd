907611100fc2511b8894bf0884fa46d54486a2eb
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-10-17 02:21:12 +0000

    Comment Correction

--- src/main.gd
+++ src/main.gd
@@ -2,7 +2,7 @@ extends Node2D
 class_name Main
 
 # ! WARNING DO NOT CHANGE THESE! WARNING !
-# 30.0 is wall offset, (1200, 800) is length of arena.
+# 20.0 is wall offset, (1200, 800) is length of arena.
 const UPLEFT_BOUND: Vector2 = Vector2.ONE * 20.0
 const DOWNRIGHT_BOUND: Vector2 = Vector2(1200, 800) - UPLEFT_BOUND
 
