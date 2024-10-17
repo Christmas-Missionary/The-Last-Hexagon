54f8c788f93079be9b45dfb222e7a74fa9280f01
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-10-17 14:38:59 +0000

    simplified legal and close button through children

--- src/legal_button.gd
+++ src/legal_button.gd
@@ -1,15 +1,9 @@
 extends Button
 
 @onready var _rect: = $LegalRect as CanvasItem
-@onready var _text: = $LegalText as CanvasItem
-@onready var _close_text: = $CloseButton as CanvasItem
 
 func _show() -> void:
 	_rect.visible = true
-	_text.visible = true
-	_close_text.visible = true
 
 func _close() -> void:
 	_rect.visible = false
-	_text.visible = false
-	_close_text.visible = false
