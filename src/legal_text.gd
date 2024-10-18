cd8e726095547447724fc98c0093fcc18252eb65
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-10-18 08:28:34 -0700

    Fixed legal_text.gd

--- src/legal_text.gd
+++ src/legal_text.gd
@@ -1,20 +1,12 @@
 extends RichTextLabel
 
-#const others: String = (
-#"Home Video Font (c) 2024 GGBotNet\n\
-#https://www.fontspace.com/home-video-font-f116641\n\
-#This work is avaliable under a Creative Commons Zero v1.0 Universal license\n\
-#https://creativecommons.org/publicdomain/zero/1.0/\n\
-#I am in no way endorsed by the copyright holder of this font."
-#)
-
 func _ready() -> void:
+	const seperator: String = "\n----------------------------------------------------------\n"
 	
 	# For other licenses from assests I use
-	# .txt file doesn't appear on export, find a way to keep it.
-	#var file: FileAccess = FileAccess.open("res://other_copyrights/FOR_THIS_PROJECT.txt", FileAccess.READ)
-	#var other_licenses: String = file.get_as_text()
-	#file.close()
+	var file: FileAccess = FileAccess.open("res://other_copyrights/FOR_THIS_PROJECT.txt", FileAccess.READ)
+	var other_licenses: String = file.get_as_text()
+	file.close()
 	
 	# Copyrights to every Godot component
 	var info: Array[Dictionary] = Engine.get_copyright_info()
@@ -32,8 +24,8 @@ func _ready() -> void:
 	
 	# Set as text, includes Godot License in first line
 	text = "Here's to every license and copyright listed below:\n" \
-		 + "I am in no way endorsed by or related to any of these copyrights holders.\n\n"\
-		 + "Godot Game Engine - MIT License\n\n" + Engine.get_license_text()\
-		 #+ "\n\nFor other assets used:\n\n" + others\
-		 + "\n\nAnd now, the copyright info of every component used in Godot:\n\n" + form_copyright_info\
-		 + "\n\nIf you wanted to see the licenses to those components, here it is:\n\n" + form_license_info
+		 + "I am in no way endorsed by or related to any of these copyrights holders."\
+		 + seperator + Engine.get_license_text()\
+		 + seperator + other_licenses\
+		 + seperator + form_copyright_info\
+		 + seperator + form_license_info
