4c006b5b54bc85ce4f9d3242d08511dec28f0f71
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-10-18 13:31:32 -0700

    comments for main.gd

--- src/main.gd
+++ src/main.gd
@@ -1,9 +1,10 @@
+## These constants have to go somewhere, might as well be here.
 extends Node2D
 class_name Main
 
-## ! WARNING DO NOT CHANGE THESE! WARNING !
-## 20.0 is wall offset, (1200, 800) is area of arena.
+## This is the upleft bound of a rect.
 const UPLEFT_BOUND: Vector2 = Vector2.ONE * 20.0
+## This is the downright (opposite) bound of a rect.
 const DOWNRIGHT_BOUND: Vector2 = Vector2(1200, 800) - UPLEFT_BOUND
 
 func _ready() -> void:
